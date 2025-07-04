bd860b2d08442988e8a8cf612a5b7b5284a9069f
Ziad Mohamed <ziad1xyz@gmail.com>
2025-07-04 23:40:52 +0300

    [ADD] initial project files

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using E_Commerce.Models;
+using E_Commerce.Services;
+using E_Commerce.Interface;
+
+namespace E_Commerce
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Initialize customer with his balance
+            var customer = new Customer("Ziad", 2000);
+            var productService = new ProductService();
+            var cartService = new CartService(customer);
+            var checkoutService = new CheckoutService(customer);
+            var menu = new MainMenu(productService, cartService, checkoutService);
+
+            Console.WriteLine("=== E-Commerce Store ===\n");
+            Console.WriteLine($"Hello, {customer.Name}. Your balance: ${customer.Balance}\n");
+
+            while (true)
+            {
+                menu.DisplayMainMenu();
+                var choice = InputHandler.GetUserInput("");
+                Console.WriteLine();
+
+                if (choice == "5")
+                {
+                    menu.HandleMenuChoice(choice);
+                    break;
+                }
+                else
+                {
+                    menu.HandleMenuChoice(choice);
+                }
+            }
+        }
+    }
+}
